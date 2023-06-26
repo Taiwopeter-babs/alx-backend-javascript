6822707f446719063d6fe12cc1127b5046ebd669
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-27 00:07:58 +0100

    iterate through an object and return a modified string

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 module.exports = function iterateThroughObject(reportWithIterator) {
-  const last = reportWithIterator.slice(-1);
-  let newString = '';
-
-  for (let ele of reportWithIterator) {
-    if (ele !== last[0]) {
-      ele += ' | ';
-    }
-    newString += ele;
-  }
-  return newString;
+  return reportWithIterator.join(' | ');
 }
