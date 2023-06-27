84925a7a0082a1f0e5f358a42791fff408bf7209
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-27 14:17:23 +0100

    handle multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+const { uploadPhoto } = require('./utils');
+const { createUser } = require('./utils');
+
+module.exports = function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const [value1, value2] = values;
+      console.log(value1.body, value2.firstName, value2.lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+};
