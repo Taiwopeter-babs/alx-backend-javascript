7358289f2d251379d67b17e3a33934a9d52a0d93
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-27 16:55:38 +0100

    catch an error

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,7 @@
 module.exports = function divideFunction(numerator, denominator) {
 
-    try {
-        if (!denominator) {
-            throw new Error('cannot divide by 0');
-        }
-        return numerator / denominator;
-    } catch (error) {
-        console.log(error);
-    }
+  if (!denominator) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
