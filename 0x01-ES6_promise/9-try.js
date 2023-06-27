7eb400ebefd6bb67cc43ecb8d1787072fb6bd612
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-27 17:40:37 +0100

    execute function in a try/catch/finally

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ module.exports = function guardrail(mathFunction) {
     res = mathFunction();
     queue.push(res);
   } catch (error) {
-    queue.push(error.message)
+    queue.push(`Error: ${error.message}`)
   } finally {
     queue.push('Guardrail was processed');
     return queue;
