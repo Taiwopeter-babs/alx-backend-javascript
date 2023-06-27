5e255c5904c9c44b82544d1f35ebfcdb3af173ec
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-27 16:26:16 +0100

    handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,14 @@
 const signUpUser = require('./4-user-promise');
 const uploadPhoto = require('./5-photo-reject');
 
-module.exports = function handleProfileSignup(firstName, lastName, filename) {
-
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((values) => console.log(values))
-}
+module.exports = async function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((values) => {
+      const results = [];
+      values.forEach((res) => results.push({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : res.reason,
+      }));
+      return results;
+    });
+};
