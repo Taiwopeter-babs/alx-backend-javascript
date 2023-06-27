e408f5b15643e99015b6a1332214a1a77a3a6fd8
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-27 14:29:52 +0100

    handle multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ const { uploadPhoto } = require('./utils');
 const { createUser } = require('./utils');
 
 module.exports = function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const [value1, value2] = values;
       console.log(`${value1.body} ${value2.firstName} ${value2.lastName}`);
