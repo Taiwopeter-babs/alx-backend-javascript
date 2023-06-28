6f3425c16a7ad86cb99f6319e134fc2179753f75
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-28 08:20:35 +0100

    [fix]: catch errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,20 @@
-const { uploadPhoto } = require('./utils');
-const { createUser } = require('./utils');
+const { uploadPhoto, createUser } = require('./utils');
 
 module.exports = async function asyncUploadUser() {
-
   const response = {};
 
-  try {
-    [response.photo, response.user] = await Promise.all([uploadPhoto(), createUser()]);
-    return response;
+  await Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      [response.photo, response.user] = values;
+    })
+    .catch(() => { [response.photo, response.user] = [null, null]; });
+  return response;
 
-  } catch (error) {
-    [response.photo, response.user] = [null, null];
-    return response;
-  }
-}
+  // try {
+  //   [response.photo, response.user] = await Promise.all([uploadPhoto(), createUser()]);
+  //   return response;
+  // } catch (error) {
+  //   [response.photo, response.user] = [null, null];
+  //   return response;
+  // }
+};
