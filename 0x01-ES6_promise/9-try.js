25ea7839cdfe1321720c00c738b4ecb6ac889cbd
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-28 08:02:41 +0100

    [fix]: return statement

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,9 +5,9 @@ module.exports = function guardrail(mathFunction) {
     res = mathFunction();
     queue.push(res);
   } catch (error) {
-    queue.push(`Error: ${error.message}`)
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
-    return queue;
   }
+  return queue;
 };
