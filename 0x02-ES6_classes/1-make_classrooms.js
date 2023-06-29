32f97c089a2f2441476dbba99ecf2a01b7997ae6
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-29 12:27:03 +0100

    use defined class

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,8 @@
 import ClassRoom from "./0-classroom.js";
 
 export default function initializeRooms() {
-
-    const array = [
-        new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)
-    ]
-    return array;
+  const array = [
+    new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)
+  ]
+  return array;
 }
