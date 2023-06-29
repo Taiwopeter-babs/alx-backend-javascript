3e2205fcc7935987c47d366c4c3a3391b5d67242
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-29 10:19:38 +0100

    add getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,53 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        try {
+            if (typeof value !== 'string') {
+                throw new TypeError('Name must be a string');
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        this._name = value;
+    }
+
+    get length() {
+        return this._length;
+    }
+    set length(value) {
+        try {
+            if (typeof value !== 'number') {
+                throw new TypeError('Length must be a number');
+            }
+            this._lentgth = value;
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    get students() {
+        return this._students;
+    }
+    set students(value) {
+
+        try {
+            for (let ele of value) {
+                if (typeof ele !== 'string') {
+                    throw new TypeError('Name must be a string');
+                }
+            }
+            this._students = value;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
