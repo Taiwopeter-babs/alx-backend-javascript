a3eda431d7c3bfac2b7794cf14c6367037a304bb
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-29 11:31:47 +0100

    implement static method and use class without inheritance

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+    constructor(amount, Currency) {
+        this._amount = amount;
+        this._currency = Currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+    set amount(value) {
+            if (typeof value !== 'number') {
+                throw new TypeError('Amount must be a number');
+            }
+            this._amount = value;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+    set currency(currency) {
+        if (! currency instanceof Currency) {
+            throw new TypeError('currency must be an instance of Currency');
+        }
+        this._currency = Currency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
