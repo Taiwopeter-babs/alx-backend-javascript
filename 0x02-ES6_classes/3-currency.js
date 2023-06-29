ce9dfc7d6897f93f2d7c756c48c3709d35ae5dc8
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-06-29 12:45:03 +0100

    add getter and setter

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,31 +1,24 @@
 export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    get code() {
-        return this._code;
-    }
-    set code(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        this._code = value;
+  get code() {
+    return this._code;
+  }
+  set code(value) {
+    this._code = value;
+  }
 
-    }
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    this._name = value;
+  }
 
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = value;
-    }
-
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,36 +1,30 @@
 import Currency from "./3-currency.js";
 
 export default class Pricing {
-    constructor(amount, Currency) {
-        this._amount = amount;
-        this._currency = Currency;
-    }
+  constructor(amount, Currency) {
+    this._amount = amount;
+    this._currency = Currency;
+  }
 
-    get amount() {
-        return this._amount;
-    }
-    set amount(value) {
-            if (typeof value !== 'number') {
-                throw new TypeError('Amount must be a number');
-            }
-            this._amount = value;
-    }
+  get amount() {
+    return this._amount;
+  }
+  set amount(value) {
+    this._amount = value;
+  }
 
-    get currency() {
-        return this._currency;
-    }
-    set currency(currency) {
-        if (! currency instanceof Currency) {
-            throw new TypeError('currency must be an instance of Currency');
-        }
-        this._currency = Currency;
-    }
+  get currency() {
+    return this._currency;
+  }
+  set currency(currency) {
+    this._currency = Currency;
+  }
 
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
