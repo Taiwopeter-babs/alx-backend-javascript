fe60dd1d96d118f18023540c41a7f9b261f53049
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-03 20:47:04 +0100

    update map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+const groceriesList = () => {
+  const itemsList = new Map();
+
+  itemsList.set('Apples', 10);
+  itemsList.set('Tomatoes', 10);
+  itemsList.set('Rice', 1);
+  itemsList.set('Pasta', 1);
+  itemsList.set('Banana', 5);
+
+  return itemsList;
+}
+
+export default groceriesList;
