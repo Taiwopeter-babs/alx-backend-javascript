e0544bcac70c791d8c4db9d04acfb437bd3d3753
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-05 23:26:40 +0100

    add all tasks

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,10 +1,10 @@
-/// <reference path=".subjects/Teacher.ts" />
-/// <reference path=".subjects/Subjects.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subjects.ts" />
 
 export const cpp = Subjects.Cpp;
 export const java = Subjects.Java;
 export const react = Subjects.React;
 
-export const cTeacher: Subjects.Teacher = {
-  experienceTeachingC: 10,
-}
+// export const cTeacher: Subjects.Teacher = {
+//   experienceTeachingC: 10,
+// }
